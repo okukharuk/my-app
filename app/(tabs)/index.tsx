65e31522a319167f08eb81227c9661b60f70b83f
VipDipp <kuxaruk03@ukr.net>
2023-05-13 23:19:35 +0300

    Lab-1: done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,75 @@
-import { StyleSheet } from 'react-native';
+import Checkbox from 'expo-checkbox';
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { RadioButton } from 'react-native-paper';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { View } from '../../components/Themed';
+
+type TShape = null | "square" | "triangle" | "circle";
 
 export default function TabOneScreen() {
+  const [shape, setShape] = React.useState<TShape>(null);
+  const [isClicked, setIsClicked] = React.useState(false);
+  const [areaClicked, setAreaClicked] = React.useState(false);
+  const [perimeterClicked, setPerimeterClicked] = React.useState(false);
+
+  const [output, setOutput] = React.useState({});
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View className="flex flex-row w-16">
+        <Checkbox
+          value={areaClicked}
+          onChange={() => setAreaClicked((prev) => !prev)}
+        />
+        <Text>Площа</Text>
+      </View>
+      <View className="flex flex-row w-16">
+        <Checkbox
+          value={perimeterClicked}
+          onChange={() => setPerimeterClicked((prev) => !prev)}
+        />
+        <Text>Периметр</Text>
+      </View>
+      <View className="flex flex-col">
+        <View className="flex flex-row items-center">
+          <RadioButton
+            onPress={() => setShape("square")}
+            value="square"
+            status={shape === "square" ? "checked" : "unchecked"}
+          />
+          <Text>Квадрат</Text>
+        </View>
+        <View className="flex flex-row items-center">
+          <RadioButton
+            onPress={() => setShape("triangle")}
+            value="triangle"
+            status={shape === "triangle" ? "checked" : "unchecked"}
+          />
+          <Text>Трикутник</Text>
+        </View>
+        <View className="flex flex-row items-center">
+          <RadioButton
+            onPress={() => setShape("circle")}
+            value="circle"
+            status={shape === "circle" ? "checked" : "unchecked"}
+          />
+          <Text>Круг</Text>
+        </View>
+      </View>
+      <View
+        onTouchEnd={() => setIsClicked(true)}
+        className="w-16 h-8 border-black border-2 flex items-center justify-center"
+      >
+        <Text>OK</Text>
+      </View>
+      {isClicked && (
+        <>
+          {areaClicked && <Text>Площа</Text>}
+          {perimeterClicked && <Text>Периметр</Text>}
+          {shape && <Text>{shape}</Text>}
+        </>
+      )}
     </View>
   );
 }
@@ -16,16 +77,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,16 +8,16 @@ import { useColorScheme } from 'react-native';
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "tabs",
 };
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -40,10 +40,10 @@ function RootLayoutNav() {
 
   return (
     <>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
         </Stack>
       </ThemeProvider>
     </>
